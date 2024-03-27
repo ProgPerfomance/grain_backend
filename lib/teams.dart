e278246247a8ad2911e125e641f47a2476b3a745
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-03-28 02:16:20 +0300

    ...

--- lib/teams.dart
+++ lib/teams.dart
@@ -2,7 +2,7 @@ import 'package:mysql_client/mysql_client.dart';
 
 Future<List> getTeams(MySQLConnection sql) async {
   List teams = [];
- final response = await sql.execute('select * from teams');
+ final response = await sql.execute('select * from users where rules = 1');
   for(var item in response.rows) {
     var data = item.assoc();
     teams.add(
