ef888a371d1a6b2bba40a091fa3b65cdacdb8bde
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-13 17:10:27 +0300

    ...

--- lib/game.dart
+++ lib/game.dart
@@ -14,12 +14,16 @@ Future<int> endGame(MySQLConnection sql, {
   required masterId,
   required winner,
 }) async {
-  var resul = await sql.execute(
-    "SELECT * FROM games",
-    {},
-  );
-  String id = resul.rows.last.assoc()['id'] as String;
-  int idInt = int.parse(id);
+  int idInt;
+  try {
+    var resul = await sql.execute(
+      "SELECT * FROM games",
+    );
+    String id = resul.rows.last.assoc()['id'] as String;
+     idInt = int.parse(id);
+  } catch (_) {
+    idInt = 0;
+  }
   sql.execute("insert into games (id, team1_id, team2_id,team3_id,team4_id,team5_id, team1_score, team2_score,team3_score,team4_score,team5_score, master_id, winner) values (${idInt+1}, $team1Id,$team2Id,$team3Id,$team4Id,$team5Id,$team1Score,$team2Score,$team3Score,$team4Score,$team5Score,$masterId,$winner)");
   return idInt+1;
 }
