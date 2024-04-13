0bec17a91881afe6e32817b3080d496865bdc419
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-13 17:11:29 +0300

    ...

--- lib/auth.dart
+++ lib/auth.dart
@@ -7,15 +7,16 @@ Future<void> createAccount(MySQLConnection sql,{
   required type,
 
 }) async{
+  int id_int;
   try {
     var resul = await sql.execute(
       "SELECT * FROM users",
     );
     String id = resul.rows.last.assoc()['id'] as String;
-    int id_int = int.parse(id);
+   id_int = int.parse(id);
   }
   catch(_) {
-
+id_int = 0;
   }
  await sql.execute("insert into users (id, name, email, password, rules,games,wins,score) values (${id_int+1}, '$name', '$email', '$password', $type, 0,0,0)");
 }
