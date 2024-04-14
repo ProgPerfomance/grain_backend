0e7ecfdfb65fb0f986ddb8368e1dd50ed8026ac6
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-04-14 04:53:59 +0300

    ...

--- bin/grain_backend.dart
+++ bin/grain_backend.dart
@@ -45,6 +45,12 @@ void main(List<String> arguments)async {
     await endGame(sql, team1Id: data['team1_id'], team2Id: data['team2_id'], team3Id: data['team3_id'], team4Id: data['team4_id'], team5Id: data['team5_id'], team1Score: data['team1_score'], team2Score: data['team2_score'], team3Score: data['team3_score'], team4Score: data['team4_score'], team5Score: data['team5_score'], masterId: data['master_id'], winner: data['winner']);
     return Response.ok('created');
   });
+  router.post('/auth', (Request request) async {
+    var json = await request.readAsString();
+    var data = await jsonDecode(json);
+   Map response = await auth(sql, data['email'], data['password']);
+    return Response.ok(jsonEncode(response));
+  });
   router.get('/questions', (Request request) async {
     List response = await getQuestions(sql);
     return Response.ok(jsonEncode(response));
--- lib/auth.dart
+++ lib/auth.dart
@@ -20,3 +20,21 @@ id_int = 0;
   }
  await sql.execute("insert into users (id, name, email, password, rules,games,wins,score) values (${id_int+1}, '$name', '$email', '$password', $type, 0,0,0)");
 }
+
+Future<Map> auth(MySQLConnection sql, email, password) async {
+  try {
+  final response =  await sql.execute("select * from users where email='$email' and password ='$password'");
+    return {
+      'success': true,
+      'uid': response.rows.first.assoc()['id'],
+      'name': response.rows.first.assoc()['name'],
+      'rules': response.rows.first.assoc()['rules'],
+    };
+  }
+  catch(_) {
+    return {
+      'success': false,
+    };
+  }
+
+}
