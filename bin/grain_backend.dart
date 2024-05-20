b5535a850e434437f907a7fc1ba2d208618a3e0c
SmartCherry <97845331+ProgPerfomance@users.noreply.github.com>
2024-05-21 00:11:16 +0300

    ...

--- bin/grain_backend.dart
+++ bin/grain_backend.dart
@@ -16,68 +16,84 @@ void main(List<String> arguments)async {
       password: '1234567890',
       databaseName: 'grade');
   await sql.connect(timeoutMs: 99999999999);
+  void checkSQL() {
+    if(sql.connected == false ){
+      sql.connect();
+    }
+  }
   Router router = Router();
   router.get('/teams', (Request request) async{
+    checkSQL();
     List response = await getTeams(sql);
     return Response.ok(jsonEncode(response));
   });
   router.post('/registration', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     createAccount(sql, name: data['name'], email: data['email'], password: data['password'], type: data['type']);
     return Response.ok('created');
   });
   router.post('/questions', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     await createQuestion(sql: sql, qText: data['q_text'], qAnswer: data['q_answer'], qDelay: data['q_delay'], qComment: data['q_comment'], masterID: data['uid'], price: data['price'], tip: data['tip']);
     return Response.ok('created');
   });
   router.post('/category', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     await createCollection(sql: sql, name: data['name']);
     return Response.ok('created');
   });
   router.post('/endGame', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     await endGame(sql, team1Id: data['team1_id'], team2Id: data['team2_id'], team3Id: data['team3_id'], team4Id: data['team4_id'], team5Id: data['team5_id'], team1Score: data['team1_score'], team2Score: data['team2_score'], team3Score: data['team3_score'], team4Score: data['team4_score'], team5Score: data['team5_score'], masterId: data['master_id'], winner: data['winner']);
     return Response.ok('created');
   });
   router.post('/auth', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
    Map response = await auth(sql, data['email'], data['password']);
     return Response.ok(jsonEncode(response));
   });
   router.get('/questions', (Request request) async {
+    checkSQL();
     List response = await getQuestions(sql);
     return Response.ok(jsonEncode(response));
   });
   router.post('/useQuest', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     Map response = await auth(sql, data['email'], data['password']);
     return Response.ok(jsonEncode(response));
   });
   router.get('/categories', (Request request) async {
+    checkSQL();
   List response = await  getCategories(sql);
   return Response.ok(jsonEncode(response));
   });
   router.post('/teamGames', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     List response = await  getTeamGames(sql,data['id']);
     return Response.ok(jsonEncode(response));
   });
   router.post('/getTeamInfo', (Request request) async {
+    checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     Map response = await  getTeamInfo(sql,data['id']);
     return Response.ok(jsonEncode(response));
   });
-  router.post('/getOwnerGames', (Request request) async {
+  router.post('/getOwnerGames', (Request request) async {checkSQL();
     var json = await request.readAsString();
     var data = await jsonDecode(json);
     List response = await  getOwnerGames(sql,data['id']);
